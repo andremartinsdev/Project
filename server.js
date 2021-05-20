3b04eb568b10fb2e24e93cabae78842d7891225d
Militao36 <matheusmoreira3693@gmail.com>
2021-05-20 10:26:33 -0300

    Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const { config } = require('dotenv')
+
+config()
+
+const configs = {
+    caminho: "dist",
+    forcarHTTPS: false,
+    port: process.env.PORT
+}
+
+if (configs.forcarHTTPS)
+    app.use((req, res, next) => {
+        if ((req.headers["x-forwarded-proto"] || "").endsWith("http"))
+            res.redirect(`https://${req.headers.host}${req.url}`);
+        else
+            next();
+    });
+
+app.use(express.static(configs.caminho));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, configs.caminho, "index.html"));
+});
+
+app.listen(configs.port, () => {
+    console.log(`Escutando na ${configs.port}!`);
+});
